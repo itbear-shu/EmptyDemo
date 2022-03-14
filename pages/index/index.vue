29f0ec563857737df8c7e742cdbb9e23ff4390b2
itbear-shu <77342779+itbear-shu@users.noreply.github.com>
2022-03-14 16:16:56 +0800

    uView1

---
+++ pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		{{vuex_user}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+			this.getAll()
+		},
+		methods: {
+			async getAll() {
+				const res = await this.$u.api.getAll()
+				console.log(res)
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
